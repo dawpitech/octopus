15f2dbe92ad60eaf6a40aff1234792f68b2d6033
Antoine <me@antoinebellanger.fr>
2025-03-31 10:55:33 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            sshpass
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
